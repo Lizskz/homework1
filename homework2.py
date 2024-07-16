5d10fc645994cd9b1596b01ec4aa88d0ba7b4e63
Lizskz <shuminec.liza@gmail.com>
2024-07-16 23:38:31 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+dz_count = 12
+hours_count = 1.5
+course_name = 'Python'
+time_for_taske = hours_count/dz_count
+print('Курс:',course_name,', всего задач:',dz_count,', затрачено часов:',hours_count,', среднее время выполнения',time_for_taske,'часа.')
